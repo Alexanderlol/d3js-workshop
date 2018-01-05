2534d57e8512a5b5427d8caeda53a5cd56daae9c
Alex Afeltra <afeltra.alex@gmail.com>
2018-01-05 03:11:14 -0500

    scatter plot intro

--- app.js
+++ app.js
@@ -182,5 +182,41 @@ var data            =   [
 var chart_width     =   800;
 var chart_height    =   400;
 
+// Create SVG element
 
+var svg = d3.select('#chart')
+		.append('svg')
+		.attr('width', chart_width)
+		.attr('height', chart_height);
+
+// Create circles
+svg.selectAll('circle')
+		.data(data)
+		.enter()
+		.append('circle')
+		.attr('cx', function(d){
+			return d[0];
+		})
+		.attr('cy', function(d){
+			return d[1];
+		})
+		.attr('r', function(d){
+			return d[1] / 10;
+		})
+		.attr('fill', '#D1AB0E');
+
+// Create labels
+svg.selectAll('text')
+		.data(data)
+		.enter()
+		.append('text')
+		.text(function(d){
+			return d.join(',');
+		})
+		.attr('x', function(d){
+			return d[0];
+		})
+		.attr('y', function(d){
+			return d[1];
+		});
 
