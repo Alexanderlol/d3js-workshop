6a702d2b2c15c87a4ca2cb9861743d7fe495e14f
Alex Afeltra <afeltra.alex@gmail.com>
2018-02-03 22:47:18 -0500

    animations and interactivity 1-3

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 /* D3 Fundamentals */
 
 /* Selections */
-/*var el = d3.select( 'body' ).append( 'p' );
-console.log(el);*/
+/* var el = d3.select( 'body' ).append( 'p' );
+console.log(el); */
 
 /* Manipulating Selections */
-/*var el = d3.select( 'body' )
+/* var el = d3.select( 'body' )
 		.append( 'p' )
 		.classed( 'foo', true )
 		.classed( 'bar', true )
 		.text( 'Hello World!' )
 		.style( 'color', 'blue' );
 
-console.log(el);*/
+console.log(el); */
 
 /* Binding Data */
-/*var dataset = [10, 20, 30, 40, 50];
+/* var dataset = [10, 20, 30, 40, 50];
 
 var el = d3.select('body')
 		.selectAll('p')
@@ -24,7 +24,7 @@ var el = d3.select('body')
 		.append('p')
 		.text("Hello World!");
 
-console.log(el);*/
+console.log(el); */
 
 /* Displaying Data */
 // var dataset = [10, 20, 30, 40, 50];
@@ -167,105 +167,105 @@ svg.selectAll('text')
 		.attr('fill', '#fff')
 		.attr('text-anchor', 'middle'); */
 
-/* Scatter Plot  w/ axis */
+// Scatter Plot  w/ axis 
 
-var data            =   [
-    [ 400, 200 ],
-    [ 210, 140 ],
-    [ 722, 300 ],
-    [ 70, 160 ],
-    [ 250, 50 ],
-    [ 110, 280 ],
-    [ 699, 225 ],
-    [ 90, 220 ]
-];
-var chart_width     =   800;
-var chart_height    =   400;
-var padding         =   50;
+// var data            =   [
+//     [ 400, 200 ],
+//     [ 210, 140 ],
+//     [ 722, 300 ],
+//     [ 70, 160 ],
+//     [ 250, 50 ],
+//     [ 110, 280 ],
+//     [ 699, 225 ],
+//     [ 90, 220 ]
+// ];
+// var chart_width     =   800;
+// var chart_height    =   400;
+// var padding         =   50;
 
-// Create SVG Element
-var svg             =   d3.select( '#chart' )
-    .append( 'svg' )
-    .attr( 'width', chart_width )
-    .attr( 'height', chart_height );
+//  Create SVG Element
+// var svg             =   d3.select( '#chart' )
+//     .append( 'svg' )
+//     .attr( 'width', chart_width )
+//     .attr( 'height', chart_height );
 
-// Create Scales
-var x_scale         =   d3.scaleLinear()
-    .domain([0, d3.max(data, function(d){
-        return d[0];
-    })])
-    .range([ padding, chart_width - padding * 2 ]);
+//  Create Scales
+// var x_scale         =   d3.scaleLinear()
+//     .domain([0, d3.max(data, function(d){
+//         return d[0];
+//     })])
+//     .range([ padding, chart_width - padding * 2 ]);
 
-var y_scale         =   d3.scaleLinear()
-    .domain([ 0, d3.max(data, function(d){
-        return d[1];
-    })])
-    .range([ chart_height - padding, padding ]);
+// var y_scale         =   d3.scaleLinear()
+//     .domain([ 0, d3.max(data, function(d){
+//         return d[1];
+//     })])
+//     .range([ chart_height - padding, padding ]);
 
-var r_scale         =   d3.scaleLinear()
-    .domain([0, d3.max(data, function( d ){
-        return d[1];
-    })])
-    .range([5, 30]);
+// var r_scale         =   d3.scaleLinear()
+//     .domain([0, d3.max(data, function( d ){
+//         return d[1];
+//     })])
+//     .range([5, 30]);
 
-var a_scale         =   d3.scaleSqrt()
-    .domain([ 0, d3.max(data, function(d) {
-        return d[1];
-    })])
-    .range([ 0, 25 ]);
+// var a_scale         =   d3.scaleSqrt()
+//     .domain([ 0, d3.max(data, function(d) {
+//         return d[1];
+//     })])
+//     .range([ 0, 25 ]);
 
- // Create Axis
-var x_axis          =   d3.axisBottom(x_scale);
-		//.ticks(5);
-		//.tickValues([0, 150, 250, 600, 700]);
+//   Create Axis
+// var x_axis          =   d3.axisBottom(x_scale);
+// 		.ticks(5);
+// 		.tickValues([0, 150, 250, 600, 700]);
 
- svg.append('g')
- 			.attr('class', 'x-axis')
- 			.attr('transform', 'translate(0,' + (chart_height - padding) + ')')
- 			.call(x_axis);
+//  svg.append('g')
+//  			.attr('class', 'x-axis')
+//  			.attr('transform', 'translate(0,' + (chart_height - padding) + ')')
+//  			.call(x_axis);
 
-var y_axis          =   d3.axisLeft(y_scale)
-		.ticks(5);
-		// .tickFormat(function(d){
-		// 	return d + '%';
-		// });
+// var y_axis          =   d3.axisLeft(y_scale)
+// 		.ticks(5);
+// 		 .tickFormat(function(d){
+// 		 	return d + '%';
+// 		 });
 
- svg.append('g')
- 			.attr('class', 'y-axis')
- 			.attr('transform', 'translate(' + padding + ', 0 )')
- 			.call(y_axis);
+//  svg.append('g')
+//  			.attr('class', 'y-axis')
+//  			.attr('transform', 'translate(' + padding + ', 0 )')
+//  			.call(y_axis);
 
-// Create Circles
-svg.selectAll( 'circle' )
-    .data( data )
-    .enter()
-    .append( 'circle' )
-    .attr("cx", function(d) {
-        return x_scale(d[0]);
-    })
-    .attr("cy", function(d) {
-        return y_scale(d[1]);
-    })
-    .attr("r", function(d){
-        return a_scale(d[1]);
-    })
-    .attr( 'fill', '#D1AB0E' );
+//  Create Circles
+// svg.selectAll( 'circle' )
+//     .data( data )
+//     .enter()
+//     .append( 'circle' )
+//     .attr("cx", function(d) {
+//         return x_scale(d[0]);
+//     })
+//     .attr("cy", function(d) {
+//         return y_scale(d[1]);
+//     })
+//     .attr("r", function(d){
+//         return a_scale(d[1]);
+//     })
+//     .attr( 'fill', '#D1AB0E' );
 
-// Create Labels
-svg.append('g')
-		.selectAll( 'text' )
-    .data( data )
-    .enter()
-    .append( 'text' )
-    .text(function(d) {
-        return d.join( ',' );
-    })
-    .attr("x", function(d) {
-        return x_scale(d[0]);
-    })
-    .attr("y", function(d) {
-        return y_scale(d[1]);
-    });
+//  Create Labels
+// svg.append('g')
+// 		.selectAll( 'text' )
+//     .data( data )
+//     .enter()
+//     .append( 'text' )
+//     .text(function(d) {
+//         return d.join( ',' );
+//     })
+//     .attr("x", function(d) {
+//         return x_scale(d[0]);
+//     })
+//     .attr("y", function(d) {
+//         return y_scale(d[1]);
+//     });
 
 /* Time Scale scatter plot */
 
@@ -288,20 +288,20 @@ svg.append('g')
 // var chart_height    =   400;
 // var padding         =   50;
 
-// // Loop through each date
+//  Loop through each date
 
 // data.forEach(function(e, i){
 // 	data[i].date = time_parse(e.date);
 // });
 
-// // Create SVG element
+//  Create SVG element
 
 // var svg = d3.select('#chart')
 // 		.append('svg')
 // 		.attr('width', chart_width)
 // 		.attr('height', chart_height);
 
-// // Create scales
+//  Create scales
 
 // var x_scale = d3.scaleTime()
 // 		.domain([
@@ -332,7 +332,7 @@ svg.append('g')
 // 		})])
 // 		.range([0, 25]);
 
-// // Create circles
+//  Create circles
 // svg.selectAll('circle')
 // 		.data(data)
 // 		.enter()
@@ -348,7 +348,7 @@ svg.append('g')
 // 		})
 // 		.attr('fill', '#D1AB0E');
 
-// // Create labels
+//  Create labels
 // svg.selectAll('text')
 // 		.data(data)
 // 		.enter()
@@ -371,7 +371,97 @@ var scale = d3.scaleLinear()
 		.domain([d3.min(slices), d3.max(slices)])
 		.range([10, 350]);*/
 
+/* *************************
+ORDINAL SCALES
+************** */
+// var fruit = ['Apples', 'Oranges', 'Grapes', 'Strawberry', 'Kiwi'];
 
+// var scale = d3.scaleBand()
+// 			.domain(d3.range(fruit.length))
+// 			.range( [ 0, 500 ] );
+
+var data            =   [6,20,21,14,2,30,7,16,25,5,11,28,10,26,9];
+
+// Create SVG Element
+var chart_width     =   800;
+var chart_height    =   400;
+var bar_padding     =   5;
+var svg             =   d3.select( '#chart' )
+    .append( 'svg' )
+    .attr( 'width', chart_width )
+    .attr( 'height', chart_height );
+
+// Create scales
+// 800 / 15 = 53.33
+var x_scale = d3.scaleBand()
+		.domain( d3.range(data.length) )
+		.rangeRound([ 0, chart_width ])
+		.paddingInner(0.05);
+
+var y_scale = d3.scaleLinear()
+		.domain([ 0, d3.max(data) ])
+		.range([ 0, chart_height ]);
+
+// Bind Data and create bars
+svg.selectAll( 'rect' )
+    .data( data )
+    .enter()
+    .append( 'rect' )
+    .attr( 'x', function( d, i ){
+        return x_scale(i);
+    })
+    .attr( 'y', function(d ){
+        return chart_height - y_scale(d);
+    })
+    .attr( 'width', x_scale.bandwidth() )
+    .attr( 'height', function( d ){
+        return y_scale(d);
+    })
+    .attr( 'fill', '#7ED26D' );
+
+// Create Labels
+svg.selectAll( 'text' )
+    .data(data)
+    .enter()
+    .append( 'text' )
+    .text(function( d ){
+        return d;
+    })
+    .attr( 'x', function( d, i ){
+        return x_scale(i) + x_scale.bandwidth() /2;
+    })
+    .attr( 'y', function(d ){
+        return chart_height - y_scale(d) + 15;
+    })
+    .attr( 'font-size', 14 )
+    .attr( 'fill', '#fff' )
+    .attr( 'text-anchor', 'middle' );
+
+// Events
+d3.select('button').on("click", function(){
+	data.reverse();
+
+	svg.selectAll('rect')
+			.data(data)
+			.attr( 'y', function(d ){
+        return chart_height - y_scale(d);
+    	})
+			.attr( 'height', function( d ){
+        return y_scale(d);
+    	});
+
+  svg.selectAll( 'text' )
+    	.data(data)
+    	.text(function( d ){
+        return d;
+    	})
+    	.attr( 'x', function( d, i ){
+        return x_scale(i) + x_scale.bandwidth() /2;
+    	})
+    	.attr( 'y', function(d ){
+        return chart_height - y_scale(d) + 15;
+    	});
+});
 
 
 
